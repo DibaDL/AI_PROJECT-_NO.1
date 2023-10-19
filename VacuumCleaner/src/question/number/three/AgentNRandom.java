845f336bfc9982e5a959eec8d4cbbe6c720857fe
Diba <elahidiba82@gmail.com>
2023-10-19 18:15:10 +0330

    Third question template is done

---
+++ VacuumCleaner/src/question/number/three/AgentNRandom.java
@@ -0,0 +1,66 @@
+package question.number.three;
+
+import question.number.two.VacuumCleanerN;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class AgentNRandom {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        VacuumCleanerNRandom vacuumCleanerNRandom = new VacuumCleanerNRandom();
+        vacuumCleanerNRandom.setN(n);
+        vacuumCleanerNRandom.setLocationsDirtState();
+        vacuumCleanerNRandom.setCurrentXAxis();
+        vacuumCleanerNRandom.setCurrentYAxis();
+
+        while (!vacuumCleanerNRandom.checkHalt()){
+            System.out.println("*******************************************************************");
+            System.out.println("Current loc : [" + vacuumCleanerNRandom.currentXAxis + "][" + vacuumCleanerNRandom.currentYAxis + "]");
+            for (int i = 0; i < vacuumCleanerNRandom.n; i++){
+                for (int j = 0; j < vacuumCleanerNRandom.n; j++){
+                    if (vacuumCleanerNRandom.locationsDirtState[i][j] == 0){
+                        System.out.print("Clean ");
+                    }else {
+                        System.out.print("Dirty ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("*******************************************************************");
+            System.out.println("In halt loop");
+            if (vacuumCleanerNRandom.locationsDirtState[vacuumCleanerNRandom.currentXAxis][vacuumCleanerNRandom.currentYAxis] == 1){
+                vacuumCleanerNRandom.suck();
+            }
+            boolean actionState = false;
+            while(!actionState){
+                int action = actionController();
+                System.out.println("In action state loop");
+                if (action == 0){
+                    System.out.println("Right");
+                    actionState = vacuumCleanerNRandom.moveRight();
+                }else if (action == 1){
+                    System.out.println("Left");
+                    actionState = vacuumCleanerNRandom.moveLeft();
+                }else if (action == 2){
+                    System.out.println("Up");
+                    actionState = vacuumCleanerNRandom.moveUp();
+                }else {
+                    System.out.println("Down");
+                    actionState = vacuumCleanerNRandom.moveDown();
+                }
+            }
+
+            vacuumCleanerNRandom.setLocationsDirtState();
+
+        }
+    }
+
+    public static int actionController(){
+        Random rand = new Random();
+        int action = rand.nextInt(4);
+        return action;
+    }
+
+}
---
+++ VacuumCleaner/src/question/number/three/VacuumCleanerNRandom.java
@@ -0,0 +1,97 @@
+package question.number.three;
+
+import java.util.Random;
+
+public class VacuumCleanerNRandom {
+    int n;
+
+    int currentXAxis;
+    int currentYAxis;
+    int[][] locationsDirtState;
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setLocationsDirtState() {
+        locationsDirtState = new int[this.n][this.n];
+        for (int i = 0; i < this.n; i++){
+            for (int j = 0; j < this.n; j++){
+                Random rand = new Random();
+                int state = rand.nextInt(2);
+                this.locationsDirtState[i][j] = state;
+            }
+        }
+    }
+
+
+    public void setCurrentXAxis(){
+        Random rand = new Random();
+        int x = rand.nextInt(this.n);
+        this.currentXAxis = x;
+    }
+
+    public void setCurrentYAxis(){
+        Random rand = new Random();
+        int y = rand.nextInt(this.n);
+        this.currentYAxis = y;
+    }
+
+    public void suck(){
+        this.locationsDirtState[this.currentXAxis][this.currentYAxis] = 0;
+    }
+
+
+    public boolean moveRight(){
+        boolean done = true;
+        if (this.currentYAxis == this.n - 1){
+            done = false;
+        }else {
+            this.currentYAxis += 1;
+        }
+        return done;
+    }
+
+    public boolean moveLeft(){
+        boolean done = true;
+        if (this.currentYAxis == 0){
+            done = false;
+        }else {
+            this.currentYAxis -= 1;
+        }
+        return done;
+    }
+
+    public boolean moveUp(){
+        boolean done = true;
+        if (this.currentXAxis == 0){
+            done = false;
+        }else {
+            this.currentXAxis -= 1;
+        }
+        return done;
+    }
+
+    public boolean moveDown(){
+        boolean done = true;
+        if (this.currentXAxis == n - 1){
+            done = false;
+        }else {
+            this.currentXAxis += 1;
+        }
+        return done;
+    }
+
+    public boolean checkHalt(){
+        boolean halt = true;
+        for (int i = 0; i < this.n; i++){
+            for (int j = 0; j < this.n; j++){
+                if(this.locationsDirtState[i][j] == 1) {
+                    halt = false;
+                    break;
+                }
+            }
+        }
+        return halt;
+    }
+}
